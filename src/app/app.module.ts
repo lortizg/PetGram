44f17645e8923af2c14e54e002f5c33192be9c05
lortizg <laura.ortiz@esail.es>
2022-10-04 16:49:20 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { StoriesComponent } from './pages/stories/stories.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    StoriesComponent,
+    SearchComponent,
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
