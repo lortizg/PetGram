26e222026db31abc249ee0e8d43a48e8b7f73e44
lortizg <laura.ortiz@esail.es>
2022-10-10 12:38:50 +0200

    valid register and login

--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,9 @@ export const defaultStory= (): IStory => {
         seenBy:[]
     }
 }
+
+export const isValidEmail = (email: string) => {
+    const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexp.test(email);
+
+}
