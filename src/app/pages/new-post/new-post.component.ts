3181e1967404041c5e64304f544ee3c13f9e24c0
lortizg <laura.ortiz@esail.es>
2022-10-11 15:41:30 +0200

    final touches

--- src/app/pages/new-post/new-post.component.ts
+++ src/app/pages/new-post/new-post.component.ts
@@ -11,17 +11,19 @@ import { UploadFileService } from 'src/app/services/upload-file.service';
   styleUrls: ['./new-post.component.scss']
 })
 export class NewPostComponent implements OnInit {
-  newPost=this.formBuilder.group({
+  newPost={
     description:'',
     image:''
-  });
-  constructor(private formBuilder:FormBuilder,private fileUploader:UploadFileService,private postManager:PostManagerService,private sessionManager:SessionManagerService,private router:Router) { }
+  };
+  constructor(private formBuilder:FormBuilder,private fileUploader:UploadFileService,private postManager:PostManagerService,private sessionManager:SessionManagerService,private router:Router) {
+    
+  }
 
   ngOnInit(): void {
   }
   public publish(){
-    const desc=this.newPost.value.description || "";
-    const image=this.newPost.value.image || "";
+    const desc=this.newPost.description || "";
+    const image=this.newPost.image || "";
     console.log(desc +": "+image);
     if(image!==""){
       this.postManager.addPost(this.sessionManager.getId(),desc,image);
@@ -32,7 +34,7 @@ export class NewPostComponent implements OnInit {
     let files=(event.target as HTMLInputElement).files;
     if(files!=null){
       let base64=await this.fileUploader.fileToBase64(files[0]) as string;
-      this.newPost.value.image=base64;
+      this.newPost.image=base64;
     }
   }
 }
